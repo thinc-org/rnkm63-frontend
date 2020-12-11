b6cc488fc49ec32e144f804f12a1c1cf91407121
Uttanon <uttanon.aug@gmail.com>
2020-12-12 01:50:42 +0700

    header and footer (not complete)

--- src/components/shell/Footer.tsx
+++ src/components/shell/Footer.tsx
@@ -1,46 +1,39 @@
-import { Box, makeStyles, Typography } from '@material-ui/core'
-import React from 'react'
-import SGCUIcon from '../../local/sgcuIcon.png'
-import THINCIcon from '../../local/thincIcon.png'
-
+import { Box, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import SGCUIcon from "../../local/sgcuIcon.png";
+import THINCIcon from "../../local/thincIcon.png";
 const useStyles = makeStyles({
   root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     border: 0,
     borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
     height: 48,
-    padding: '0 30px',
+    padding: "0 30px",
   },
   footer: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   sgcuIcon: {
-    width: '25.63px',
-    height: '45px',
+    width: "25.63px",
+    height: "45px",
   },
   thincIcon: {
-    marginLeft: '12.37px',
-    width: '99.09px',
-    height: '40px',
+    marginLeft: "12.37px",
+    width: "99.09px",
+    height: "40px",
   },
   footerFont: {
-    color: '#8D8D8D',
-    textAlign: 'center',
-    fontSize: '10px',
-    fontFamily: 'Kanit',
+    color: "#8D8D8D",
+    textAlign: "center",
+    fontSize: "10px",
+    marginBottom: "9px",
   },
-  footerFontLastLine: {
-    color: '#8D8D8D',
-    textAlign: 'center',
-    fontSize: '10px',
-    fontFamily: 'Kanit',
-    marginBottom: '9px',
-  },
-})
+});
 function Footer() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <Box className={classes.footer}>
       <Box display="flex" flexDirection="column" marginTop="15px">
@@ -55,10 +48,8 @@ function Footer() {
         </Box>
         <Typography variant="body1" className={classes.footerFont}>
           งานรับน้องก้าวใหม่ ปีการศึกษา 2563
-          องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย
-        </Typography>
-        <Typography variant="body1" className={classes.footerFontLastLine}>
-          สงวนลิขสิทธิ์ © 2563 Thinc.
+          องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย สงวนลิขสิทธิ์ © 2563
+          Thinc.
         </Typography>
       </Box>
     </Box>
--- src/components/shell/Header.tsx
+++ src/components/shell/Header.tsx
@@ -1,19 +1,21 @@
-import { Box, makeStyles } from '@material-ui/core'
-import React from 'react'
-import { Loading } from '../common'
-import LanguageSwitcher from './misc/LanguageSwitcher'
-import LogOutButton from './misc/LogOutButton'
-import THINCLogo from '../../local/thincLogo.png'
+import { Box, Button, makeStyles } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Loading } from "../common";
+import LanguageSwitcher from "./misc/LanguageSwitcher";
+import THINCLogo from "../../local/thincLogo.png";
+import { UserContext } from "../../contexts/UserContext";
 import { withSuspense } from '../hoc'
 
 const useStyles = makeStyles({
   header: {
-    marginTop: '20px',
+    marginTop: "20px",
   },
   thincLogo: {},
-})
+});
 function Header() {
-  const classes = useStyles()
+  const classes = useStyles();
+  const user = React.useContext(UserContext)[0];
   return (
     <Box className={classes.header}>
       <Box display="flex" flexDirection="row" justifyContent="space-between">
