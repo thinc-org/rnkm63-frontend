d687dcc88245d79dca6457e99a57e0b2c35e0ecd
Uttanon <uttanon.aug@gmail.com>
2020-12-12 01:50:42 +0700

    header and footer (not complete)

--- src/components/shell/Footer.tsx
+++ src/components/shell/Footer.tsx
@@ -1,20 +1,59 @@
-import React from 'react'
-import { Box, makeStyles } from '@material-ui/core'
-
+import { Box, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import SGCUIcon from "../../local/sgcuIcon.png";
+import THINCIcon from "../../local/thincIcon.png";
 const useStyles = makeStyles({
-  Footer: {
-    minHeight: '8vh',
-    border: '2px solid red',
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
   },
-})
-
+  footer: {
+    backgroundColor: "black",
+  },
+  sgcuIcon: {
+    width: "25.63px",
+    height: "45px",
+  },
+  thincIcon: {
+    marginLeft: "12.37px",
+    width: "99.09px",
+    height: "40px",
+  },
+  footerFont: {
+    color: "#8D8D8D",
+    textAlign: "center",
+    fontSize: "10px",
+    marginBottom: "9px",
+  },
+});
 function Footer() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
-    <Box className={classes.Footer}>
-      <h1>Footer GOES HERE</h1>
-    </Box>
-  )
+    <div className={classes.footer}>
+      <Box display="flex" flexDirection="column" marginTop="15px">
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          marginBottom="6px"
+        >
+          <img src={SGCUIcon} alt="" className={classes.sgcuIcon} />
+          <img src={THINCIcon} alt="" className={classes.thincIcon} />
+        </Box>
+        <Typography variant="body1" className={classes.footerFont}>
+          งานรับน้องก้าวใหม่ ปีการศึกษา 2563
+          องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย สงวนลิขสิทธิ์ © 2563
+          Thinc.
+        </Typography>
+      </Box>
+    </div>
+  );
 }
 
 export default Footer
--- src/components/shell/Header.tsx
+++ src/components/shell/Header.tsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Box, Button, makeStyles } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Loading } from "../common";
+import LanguageSwitcher from "./misc/LanguageSwitcher";
+import THINCLogo from "../../local/thincLogo.png";
+import { UserContext } from "../../contexts/UserContext";
 
-import { Box, makeStyles } from '@material-ui/core'
-
-import { withSuspense } from '../hoc'
-import LanguageSwitcher from './misc/LanguageSwitcher'
-import { UserContext } from '../../contexts/UserContext'
-
-const useStyles = makeStyles({
-  Header: {
-    minHeight: '8vh',
-    border: '2px solid red',
-  },
-})
-
-function Header() {
-  const { user } = React.useContext(UserContext)
-  const classes = useStyles()
+function HeaderSuspenseHOC() {
   return (
-    <Box className={classes.Header}>
-      <h1>HEADER GOES HERE</h1>
-
-      {
-        /*
-          This is an example of how you might check if the user is logged in
-          The <Link> component should also be very useful. Check out <NavLink> too!
-          */
-        user ? (
-          <h5>{`Logged in as ${user.name}`}</h5>
-        ) : (
-          <Link to="/login">Login</Link>
-        )
-      }
-      <LanguageSwitcher />
-    </Box>
+    <React.Suspense fallback={Loading}>
+      <Header />
+    </React.Suspense>
+  )
+}
+const useStyles = makeStyles({
+  header: {
+    marginTop: "20px",
+  },
+  thincLogo: {},
+});
+function Header() {
+  const classes = useStyles();
+  const user = React.useContext(UserContext)[0];
+  return (
+    <div className={classes.header}>
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <img src={THINCLogo} alt="" className={classes.thincLogo} />
+        <Box flexDirection="row" justifyContent="flex-start">
+          <LanguageSwitcher />
+          <Button variant="contained" color="primary">
+            ออกจากระบบ
+          </Button>
+        </Box>
+      </Box>
+    </div>
   )
 }
 
