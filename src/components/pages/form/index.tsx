b0d6dbee00ce17a89c8caf4f66ec27167c3accec
PKhing <apimrak@gmail.com>
2020-12-12 00:30:28 +0700

    initial work on Feat page

--- src/components/pages/form/index.tsx
+++ src/components/pages/form/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from "react";
+import "./Form.css";
+import Button from "@material-ui/core/Button";
+import Photo from "./Image";
+import FormInput from "./FormInput";
 
 import { useTranslation } from 'react-i18next'
 
@@ -7,10 +11,14 @@ import { Box } from '@material-ui/core'
 function Form() {
   const { t } = useTranslation('form')
   return (
-    <Box>
-      <h1>{t('title')}</h1>
-    </Box>
-  )
+    <div className="Form">
+      <p>{t("title")}</p>
+      <Photo />
+      <FormInput />
+      <p>{t("submitNote")}</p>
+    </div>
+  );
+      <Button>{t("submit")}</Button>
 }
 
 export default Form
