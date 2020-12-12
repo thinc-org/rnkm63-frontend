76a350a1411386a91f5b4d200bd6e17b1dfd0c83
PoraRee <ree_inter5@hotmail.com>
2020-12-12 21:53:42 +0700

    Add LogOutButton.

--- src/components/shell/Header.tsx
+++ src/components/shell/Header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Loading } from "../common";
 import LanguageSwitcher from "./misc/LanguageSwitcher";
+import LogOutButton from "./misc/LogOutButton";
 import THINCLogo from "../../local/thincLogo.png";
 import { UserContext } from "../../contexts/UserContext";
 import { withSuspense } from '../hoc'
