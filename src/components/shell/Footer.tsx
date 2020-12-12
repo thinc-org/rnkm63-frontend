596443f1ae1c542c9949e5207297312a5e007f2b
Uttanon <uttanon.aug@gmail.com>
2020-12-12 23:48:21 +0700

    rebase

--- src/components/shell/Footer.tsx
+++ src/components/shell/Footer.tsx
@@ -1,47 +1,45 @@
-import { Box, makeStyles, ThemeProvider, Typography } from "@material-ui/core";
-import React from "react";
-import { useTranslation } from "react-i18next";
-import SGCUIcon from "../../local/sgcuIcon.png";
-import THINCIcon from "../../local/thincIcon.png";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { Box, makeStyles, Typography } from '@material-ui/core'
+import React from 'react'
+import SGCUIcon from '../../local/sgcuIcon.png'
+import THINCIcon from '../../local/thincIcon.png'
 const useStyles = makeStyles({
   root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     border: 0,
     borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
     height: 48,
-    padding: "0 30px",
+    padding: '0 30px',
   },
   footer: {
-    backgroundColor: "black",
+    backgroundColor: 'black',
   },
   sgcuIcon: {
-    width: "25.63px",
-    height: "45px",
+    width: '25.63px',
+    height: '45px',
   },
   thincIcon: {
-    marginLeft: "12.37px",
-    width: "99.09px",
-    height: "40px",
+    marginLeft: '12.37px',
+    width: '99.09px',
+    height: '40px',
   },
   footerFont: {
-    color: "#8D8D8D",
-    textAlign: "center",
-    fontSize: "10px",
-    fontFamily: "Kanit",
+    color: '#8D8D8D',
+    textAlign: 'center',
+    fontSize: '10px',
+    fontFamily: 'Kanit',
   },
   footerFontLastLine: {
-    color: "#8D8D8D",
-    textAlign: "center",
-    fontSize: "10px",
-    fontFamily: "Kanit",
-    marginBottom: "9px",
+    color: '#8D8D8D',
+    textAlign: 'center',
+    fontSize: '10px',
+    fontFamily: 'Kanit',
+    marginBottom: '9px',
   },
-});
+})
 function Footer() {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <div className={classes.footer}>
       <Box display="flex" flexDirection="column" marginTop="15px">
@@ -54,18 +52,16 @@ function Footer() {
           <img src={SGCUIcon} alt="" className={classes.sgcuIcon} />
           <img src={THINCIcon} alt="" className={classes.thincIcon} />
         </Box>
-        <ThemeProvider theme={theme}>
-          <Typography variant="body1" className={classes.footerFont}>
-            งานรับน้องก้าวใหม่ ปีการศึกษา 2563
-            องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย
-          </Typography>
-          <Typography variant="body1" className={classes.footerFontLastLine}>
-            สงวนลิขสิทธิ์ © 2563 Thinc.
-          </Typography>
-        </ThemeProvider>
+        <Typography variant="body1" className={classes.footerFont}>
+          งานรับน้องก้าวใหม่ ปีการศึกษา 2563
+          องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย
+        </Typography>
+        <Typography variant="body1" className={classes.footerFontLastLine}>
+          สงวนลิขสิทธิ์ © 2563 Thinc.
+        </Typography>
       </Box>
     </div>
-  );
+  )
 }
 
 export default Footer
--- src/components/shell/misc/LanguageSwitcher.tsx
+++ src/components/shell/misc/LanguageSwitcher.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
-import { useTranslation } from "react-i18next";
-import { Card } from "@material-ui/core";
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import { useTranslation } from 'react-i18next'
 
 function LanguageSwitcher() {
   const { t, i18n } = useTranslation("shell");
@@ -10,14 +9,14 @@ function LanguageSwitcher() {
     if (i18n.language === "en") {
       i18n.changeLanguage("th");
     } else {
-      i18n.changeLanguage("en");
+      i18n.changeLanguage('en')
     }
   }, [i18n]);
   return (
     <Button onClick={switchLng} variant="contained" color="primary">
-      {t("switchLanguage")}
+      {t('switchLanguage')}
     </Button>
-  );
+  )
 }
 
-export default LanguageSwitcher;
+export default LanguageSwitcher
