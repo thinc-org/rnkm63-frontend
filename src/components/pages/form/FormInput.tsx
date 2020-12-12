65e6ce8e299c48cc4cc5cec5c830f4eaa28c3989
PKhing <apimrak@gmail.com>
2020-12-12 13:45:32 +0700

    Make Form page responsive, Setup some variable

--- src/components/pages/form/FormInput.tsx
+++ src/components/pages/form/FormInput.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function FormInput() {
-  return <p>Inputttt</p>;
+function FormInput(props: any) {
+  return (
+    <>
+      <p>{props.userData.name}</p>
+      <p>{props.userData.surname}</p>
+      <button
+        onClick={() => {
+          props.setUserData({ ...props.userData, name: "Honkai" });
+        }}
+      >
+        set data (test)
+      </button>
+    </>
+  );
 }
 
 export default FormInput;
--- src/components/pages/form/index.tsx
+++ src/components/pages/form/index.tsx
@@ -1,24 +1,88 @@
 import React from "react";
-import "./Form.css";
-import Button from "@material-ui/core/Button";
-import Photo from "./Image";
+import { useState } from "react";
+import { Button, Container, makeStyles } from "@material-ui/core";
+import Image from "./Image";
 import FormInput from "./FormInput";
-
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from "react-i18next";
 
 import { Box } from '@material-ui/core'
 
+const useStyles = makeStyles((theme: any) => ({
+  submitButton: {
+    background: "#44AD53",
+    borderRadius: "40px",
+    width: "100%",
+    maxWidth: "434px",
+    display: "block",
+    margin: "auto",
+    "&:hover": {
+      backgroundColor: "#44AD53",
+    },
+  },
+  formInput: {
+    background: "#5c5c5c",
+    [theme.breakpoints.up("sm")]: {
+      display: "inline",
+      marginLeft: "20px",
+      flexGrow: 1,
+    },
+  },
+  content: {
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+      justifyContent: "center",
+    },
+  },
+  submitNote: {
+    display: "block",
+    textAlign: "center",
+    margin: "0px",
+  },
+  container: {
+    [theme.breakpoints.up("sm")]: {
+      paddingLeft: "50px",
+      paddingRight: "50px",
+    },
+    background: "#929292",
+  },
+}));
+
 function Form() {
-  const { t } = useTranslation('form')
+  const [imageUrl, setImageUrl] = useState(
+    "https://media.discordapp.net/attachments/780977351428931594/784451408275046460/ElL3lTKVkAA2CmN.png"
+  );
+  const [userData, setUserData] = useState({
+    name: "Genshin",
+    surname: "Impact",
+  });
+  const style = useStyles();
+  const { t, i18n } = useTranslation("form");
   return (
-    <div className="Form">
+    <Container maxWidth="lg" classes={{ root: style.container }}>
       <p>{t("title")}</p>
-      <Photo />
-      <FormInput />
-      <p>{t("submitNote")}</p>
-    </div>
+      <div className={style.content}>
+        <div>
+          <Image imageUrl={imageUrl} setImageUrl={setImageUrl} />
+        </div>
+        <div className={style.formInput}>
+          <FormInput userData={userData} setUserData={setUserData} />
+        </div>
+      </div>
+      <div>
+        <Button
+          classes={{ root: style.submitButton }}
+          onClick={() => submit(imageUrl, userData)}
+        >
+          {t("submit")}
+        </Button>
+        <p className={style.submitNote}>{t("submitNote")}</p>
+      </div>
+    </Container>
   );
       <Button>{t("submit")}</Button>
 }
 
+const submit = (imageUrl: any, userData: any) => {
+  alert(imageUrl + "\n" + JSON.stringify(userData));
+};
 export default Form
