d5eb29274f6797c0511c36ebd6c1aaca8fc931de
Palm <palm.nichapat>
2020-12-12 22:26:58 +0700

    use makestyle instead .css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
 
 import { Header, Footer } from "./components/shell";
 import { Profile, Form, Login, NotFound } from "./components/pages";
 
 import { UserProvider } from "./contexts/UserContext";
+import { Container, Box, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  AppContainer: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+  },
+  PageContainer: {
+    flex: 1,
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <UserProvider>
       <Router>
-        <div className="AppContainer">
+        <Box className={classes.AppContainer}>
           <Header />
-          <div className="PageContainer">
+          <Container className={classes.PageContainer}>
             <Switch>
-              <Route path="/login">
+              <Route path="/login" exact>
                 <Login />
               </Route>
-              <Route path="/form">
+              <Route path="/form" exact>
                 <Form />
               </Route>
               <Route path="/" exact>
@@ -28,9 +40,9 @@ function App() {
                 <NotFound />
               </Route>
             </Switch>
-          </div>
+          </Container>
           <Footer />
-        </div>
+        </Box>
       </Router>
     </UserProvider>
   );
