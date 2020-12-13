64f57628225ab35445b3183e6b81306d7e6cbb17
Wisha Wa <wisha.wa@yandex.com>
2020-12-13 06:24:14 +0000

    use memoization for LanguageSwitcher

--- src/components/shell/misc/LanguageSwitcher.tsx
+++ src/components/shell/misc/LanguageSwitcher.tsx
@@ -4,13 +4,14 @@ import { useTranslation } from "react-i18next";
 
 function LanguageSwitcher() {
   const { t, i18n } = useTranslation("shell");
-  const switchLng = () => {
+
+  const switchLng = React.useCallback(() => {
     if (i18n.language === "en") {
       i18n.changeLanguage("th");
     } else {
       i18n.changeLanguage("en");
     }
-  };
+  }, [i18n]);
   return (
     <Button onClick={switchLng} variant="contained" color="primary">
       {t("switchLanguage")}
