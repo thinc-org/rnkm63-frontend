6f02927c8a974666de53af68b8c39b72ab4fa5ad
miello <partearth1212@gmail.com>
2020-12-13 22:05:09 +0700

    Modify image cropper making it easily use

--- src/components/pages/form/style/styles.tsx
+++ src/components/pages/form/style/styles.tsx
@@ -1,82 +1,112 @@
-import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
 
-export const indexStyle = makeStyles((theme: any) => ({
+const indexStyle = makeStyles((theme: any) => ({
   submitButton: {
-    background: "#44AD53",
-    borderRadius: "40px",
-    width: "100%",
-    maxWidth: "434px",
-    display: "block",
-    margin: "auto",
-    "&:hover": {
-      backgroundColor: "#44AD53",
+    background: '#44AD53',
+    borderRadius: '40px',
+    width: '100%',
+    maxWidth: '434px',
+    display: 'block',
+    margin: 'auto',
+    '&:hover': {
+      backgroundColor: '#44AD53',
     },
   },
   formInput: {
-    background: "#5c5c5c",
-    [theme.breakpoints.up("sm")]: {
-      display: "inline",
-      marginLeft: "20px",
+    background: '#5c5c5c',
+    [theme.breakpoints.up('sm')]: {
+      display: 'inline',
+      marginLeft: '20px',
       flexGrow: 1,
     },
   },
   content: {
-    [theme.breakpoints.up("sm")]: {
-      display: "flex",
-      justifyContent: "center",
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+      justifyContent: 'center',
     },
   },
   submitNote: {
-    display: "block",
-    textAlign: "center",
-    margin: "0px",
+    display: 'block',
+    textAlign: 'center',
+    margin: '0px',
   },
   container: {
-    [theme.breakpoints.up("sm")]: {
-      paddingLeft: "50px",
-      paddingRight: "50px",
+    [theme.breakpoints.up('sm')]: {
+      paddingLeft: '50px',
+      paddingRight: '50px',
     },
-    background: "#929292",
+    background: '#929292',
   },
-}));
+}))
 
-export const imageStyle = makeStyles((theme: Theme) =>
+const imageStyle = makeStyles((theme: Theme) =>
   createStyles({
     input: {
-      display: "none",
+      display: 'none',
     },
     button: {
-      backgroundColor: "#1D324D",
-      border: "1px solid #C6C6C6",
-      borderRadius: "20.2141px",
-      margin: "auto",
-      width: "150px",
-      "&:hover": {
-        backgroundColor: "#1D324D",
+      backgroundColor: '#1D324D',
+      border: '1px solid #C6C6C6',
+      borderRadius: '20.2141px',
+      margin: 'auto',
+      width: '150px',
+      '&:hover': {
+        backgroundColor: '#1D324D',
       },
     },
     submitButton: {
       margin: theme.spacing(2),
+      alignSelf: 'center',
+      width: '60%',
+      backgroundColor: '#44AD53',
+      '&:hover': {
+        background: '#44AD53',
+      },
     },
     image: {
-      borderRadius: "30px",
-      borderColor: "red",
-      borderWidth: "5px",
-      width: "150px",
-      height: "200px",
-      backgroundColor: "#C4C4C4",
-      marginBottom: "15px",
+      borderRadius: '30px',
+      borderColor: 'red',
+      borderWidth: '5px',
+      width: '150px',
+      height: '200px',
+      background: '#C4C4C4',
+      marginBottom: '15px',
     },
     reasonText: {
-      color: "#EB5757",
+      color: '#EB5757',
     },
     closeButton: {
       margin: 0,
       padding: theme.spacing(2),
-      position: "absolute",
+      position: 'absolute',
       right: theme.spacing(1),
       top: theme.spacing(1),
       color: theme.palette.grey[500],
     },
+    cropContainer: {
+      position: 'relative',
+      display: 'flex',
+      alignItems: 'center',
+      alignSelf: 'center',
+      width: '70%',
+      height: 400,
+      background: '#333',
+      [theme.breakpoints.down('sm')]: {
+        height: 400,
+      },
+    },
+    unorderList: {},
+    dialogText: {
+      color: 'black',
+      width: '70%',
+      alignSelf: 'center',
+      fontWeight: 500,
+      fontSize: '16px',
+      lineHeight: '24px',
+    },
+    dialog: {},
   })
-);
+)
+
+export { imageStyle, indexStyle }
