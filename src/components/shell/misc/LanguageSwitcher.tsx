3cf760755bebe268131cd88f18030b4151057052
Wisha Wa <wisha.wa@yandex.com>
2020-12-13 16:08:30 +0000

    a small fix with the language switcher.

--- src/components/shell/misc/LanguageSwitcher.tsx
+++ src/components/shell/misc/LanguageSwitcher.tsx
@@ -3,15 +3,15 @@ import Button from '@material-ui/core/Button'
 import { useTranslation } from 'react-i18next'
 
 function LanguageSwitcher() {
-  const { t, i18n } = useTranslation("shell");
+  const { t, i18n } = useTranslation('shell')
 
   const switchLng = React.useCallback(() => {
-    if (i18n.language === "en") {
-      i18n.changeLanguage("th");
-    } else {
+    if (i18n.language.startsWith('th')) {
       i18n.changeLanguage('en')
+    } else {
+      i18n.changeLanguage('th')
     }
-  }, [i18n]);
+  }, [i18n])
   return (
     <Button onClick={switchLng} variant="contained" color="primary">
       {t('switchLanguage')}
