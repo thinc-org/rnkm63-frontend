e3c80c333e1784d87ecdea8352edd3288c802cfe
Uttanon <uttanon.aug@gmail.com>
2020-12-13 20:34:33 +0700

    add breakpoint (also add temp font)

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -4,6 +4,15 @@ const theme = createMuiTheme({
   typography: {
     fontFamily: ['Kanit', 'Rubik', 'sans-serif'].join(','),
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
   palette: { primary: { main: '#44AD53' } },
 })
 export default theme
