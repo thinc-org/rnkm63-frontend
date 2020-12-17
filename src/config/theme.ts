3e71d6020e9db51becc948b460839c8fcd5740d7
Wisha Wa <wisha.wa@yandex.com>
2020-12-17 04:50:01 +0000

    workaround missing zero breakpoint

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,15 +1,30 @@
 import { createMuiTheme } from '@material-ui/core'
 
+// TODO: Remove zero breakpoint and move xs to zero.
+
+declare module '@material-ui/core/styles/createBreakpoints' {
+  interface BreakpointOverrides {
+    zero: true
+    xs: true
+    sm: true
+    md: true
+    lg: true
+    xl: true
+  }
+}
+
 const theme = createMuiTheme({
   typography: {
     fontFamily: ['Rubik', 'Kanit', 'sans-serif'].join(','),
   },
   breakpoints: {
+    keys: ['zero', 'xs', 'sm', 'md', 'lg', 'xl'],
     values: {
+      zero: 0,
       xs: 320,
       sm: 720,
       md: 1024,
-      lg: 1280,
+      lg: 1440,
       xl: 1920,
     },
   },
