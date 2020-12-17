db1981f23f8c517aa7f8839f1afd3d87e29b71b6
paphonb <paphonb@gmail.com>
2020-12-17 23:52:24 +0700

    Move locale files to main bundle

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,24 +1,24 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
+import LanguageDetector from 'i18next-browser-languagedetector'
+import en from './locales/en'
+import th from './locales/th'
 
 i18n
-  // load translation using http -> see /public/locales
-  .use(Backend)
   // detect user language
   .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   .init({
-    fallbackLng: "en",
+    fallbackLng: 'en',
     debug: true,
+    resources: { en, th },
 
     interpolation: {
       escapeValue: false,
     },
-  });
+  })
 
-export default i18n;
+export default i18n
---
+++ src/locales/en/index.ts
@@ -0,0 +1,27 @@
+import drawer from './drawer.json'
+import footer from './footer.json'
+import form from './form.json'
+import formComplete from './formComplete.json'
+import login from './login.json'
+import logoutbutton from './logoutbutton.json'
+import not104 from './not104.json'
+import notfound from './notfound.json'
+import profile from './profile.json'
+import shell from './shell.json'
+import tohomebutton from './tohomebutton.json'
+
+const en = {
+  drawer,
+  footer,
+  form,
+  formComplete,
+  login,
+  logoutbutton,
+  not104,
+  notfound,
+  profile,
+  shell,
+  tohomebutton,
+}
+
+export default en
