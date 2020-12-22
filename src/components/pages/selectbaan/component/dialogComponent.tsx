f85b6b31a66501d2db13a1db37658152b63efe79
CaQtiml <lsivakorn@gmail.com>
2020-12-22 21:25:49 +0700

    -

--- src/components/pages/selectbaan/component/dialogComponent.tsx
+++ src/components/pages/selectbaan/component/dialogComponent.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import {
-  createMuiTheme,
-  createStyles,
-  WithStyles,
-  makeStyles,
-  Theme,
-} from '@material-ui/core/styles'
+import { createStyles, WithStyles, Theme } from '@material-ui/core/styles'
 import MuiDialogTitle from '@material-ui/core/DialogTitle'
-import MuiDialogContent from '@material-ui/core/DialogContent'
-import MuiDialogActions from '@material-ui/core/DialogActions'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import { withStyles } from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core'
-import { green } from '@material-ui/core/colors'
 
 const styles = (theme: Theme) =>
   createStyles({
