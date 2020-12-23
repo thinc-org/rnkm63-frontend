c9d501efad32739909e06b6c3190ac440fa7f19b
Pimrak Apiratchatanont <apimrak@gmail.com>
2020-12-23 21:10:55 +0700

    fix ESLint Warning

--- src/components/pages/profile/index.tsx
+++ src/components/pages/profile/index.tsx
@@ -2,11 +2,11 @@ import { Box, Typography } from '@material-ui/core'
 import { HandleRequestError, RequestError } from 'components/common/Error'
 import Loading from 'components/common/Loading'
 import { UserContext } from 'contexts/UserContext'
+import { getBaan } from 'local/BaanInfo'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Redirect } from 'react-router-dom'
 
-import { getBaan } from 'local/BaanInfo'
 import Countdown from './Countdown'
 import { profileStyles } from './styles/profileStyles'
 
