d8694a9d941964a9bca14a93e1a928d9437cdaab
miello <partearth1212@gmail.com>
2021-01-02 00:44:35 +0700

    Remove redundant code and improve image quality

--- src/components/pages/form/utils/imageHelper.ts
+++ src/components/pages/form/utils/imageHelper.ts
@@ -6,15 +6,10 @@ export const createImage = (url: string): Promise<HTMLImageElement> =>
     const image = new Image()
     image.addEventListener('load', () => resolve(image))
     image.addEventListener('error', (error) => reject(error))
-    image.setAttribute('crossOrigin', 'anonymous') // needed to avoid cross-origin issues on CodeSandbox
     image.src = url
   })
 
 const limitFileKB = 300
-
-function getRadianAngle(degreeValue: number) {
-  return (degreeValue * Math.PI) / 180
-}
 
 export async function getResizedImage(
   img: File | Blob,
@@ -32,12 +27,14 @@ export async function getResizedImage(
         widthTarget,
         heightTarget,
         'jpeg',
-        85,
+        95,
         0,
         (uri) => {
           resolve(uri)
         },
-        'blob'
+        'blob',
+        widthTarget,
+        heightTarget
       )
     })
   }
@@ -71,7 +68,9 @@ export async function getResizedImage(
       (uri) => {
         resolve({ uri, widthImg, heightImg })
       },
-      'blob'
+      'blob',
+      widthImg,
+      heightImg
     )
   })
 }
@@ -94,45 +93,24 @@ function canvasToBlob(canvas: HTMLCanvasElement, type: string): Promise<Blob> {
  * @param {Object} pixelCrop - pixelCrop Object provided by react-easy-crop
  * @param {number} rotation - optional rotation parameter
  */
-export async function getCroppedImg(
-  imageSrc: any,
-  pixelCrop: any,
-  rotation = 0
-) {
+export async function getCroppedImg(imageSrc: any, pixelCrop: any) {
   const image = await createImage(imageSrc)
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')!!
 
-  const maxSize = Math.max(image.width, image.height)
-  const safeArea = maxSize
-
-  // set each dimensions to double largest dimension to allow for a safe area for the
-  // image to rotate in without being clipped by canvas context
-  canvas.width = safeArea
-  canvas.height = safeArea
-
-  // translate canvas context to a central location on image to allow rotating around the center.
-  ctx.translate(safeArea / 2, safeArea / 2)
-  ctx.rotate(getRadianAngle(rotation))
-  ctx.translate(-safeArea / 2, -safeArea / 2)
-
-  // draw rotated image and store data.
-  ctx.drawImage(
-    image,
-    safeArea / 2 - image.width * 0.5,
-    safeArea / 2 - image.height * 0.5
-  )
-  const data = ctx.getImageData(0, 0, safeArea, safeArea)
-
-  // set canvas width to final desired crop size - this will clear existing context
   canvas.width = pixelCrop.width
   canvas.height = pixelCrop.height
 
-  // paste generated rotate image with correct offsets for x,y crop values.
-  ctx.putImageData(
-    data,
-    Math.round(0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x),
-    Math.round(0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y)
+  ctx.drawImage(
+    image,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height
   )
 
   // As Base64 string
