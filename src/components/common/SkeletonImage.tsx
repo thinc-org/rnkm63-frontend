04cb65438379eb8678cee0de94e7bd4c518fc0b9
Saenyakorn <saneyakorn@gmail.com>
2021-01-02 17:09:38 +0700

    fix: sending props to img tag

--- src/components/common/SkeletonImage.tsx
+++ src/components/common/SkeletonImage.tsx
@@ -1,11 +1,11 @@
 import { Skeleton } from '@material-ui/lab'
 import React, { useCallback, useState } from 'react'
 
-interface SkeletionImageProps
+export interface SkeletionImageProps
   extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
 const SkeletonImage = (props: SkeletionImageProps) => {
-  const { alt, src, className, ...rest } = props
+  const { alt, className, ...rest } = props
   const [loaded, setLoaded] = useState(true)
   const handleLoaded = useCallback(() => setLoaded(false), [])
 
@@ -14,7 +14,6 @@ const SkeletonImage = (props: SkeletionImageProps) => {
       {loaded && <Skeleton variant="rect" className={className} />}
       <img
         alt={alt}
-        src={src}
         className={className}
         onLoad={handleLoaded}
         style={{ display: loaded ? 'none' : 'block' }}
